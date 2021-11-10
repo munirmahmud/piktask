82ccf4627fd84373c1c1714b20cfd17bad9aeb41
prantoshikder <prantoshikder24@gmail.com>
2021-11-10 17:25:17 +0600

    Add new total count history component

---
+++ src/components/ui/dashboard/contributor/TotalCountHistory/index.jsx
@@ -0,0 +1,70 @@
+import { Card, Grid } from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import useStyles from "./TotalCountHistory.style";
+
+const TotalCountHistory = () => {
+  const classes = useStyles();
+  const user = useSelector((state) => state.user);
+
+  const [totalSummary, setTotalSummery] = useState({});
+
+  useEffect(() => {
+    // Total earning summary API integration
+    if (user?.isLoggedIn && user?.role === "contributor") {
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/contributor/earning/summary`, 
+          {headers: { Authorization: user?.token },}
+        )
+        .then(({ data }) => {
+          if (data?.status) {
+            setTotalSummery(data?.summery);
+          }
+        });
+    }
+  }, [user?.isLoggedIn, user?.role, user?.token])
+
+  return (
+    <div className={classes.v}>
+      <Grid container spacing={0}>
+        <Grid item xs={12}>
+          <Card className={classes.cardWrapper}>
+            <div className={classes.graphBox}>
+              <div className={classes.amount}>
+                {totalSummary?.total_earning}$
+              </div>
+              <span className={classes.title}>Total Earning</span>
+            </div>
+            <div className={classes.graphBox}>
+              <span
+                className={`${classes.amount} ${classes.paidDownloadColor}`}
+              >
+                {totalSummary?.total_images}
+              </span>
+              <span className={classes.title}>Total Files</span>
+            </div>
+            <div className={classes.graphBox}>
+              <span
+                className={`${classes.amount} ${classes.freeDownloadColor}`}
+              >
+                {totalSummary?.total_followers}
+              </span>
+              <span className={classes.title}>Total Follower</span>
+            </div>
+            <div className={classes.graphBox}>
+              <span
+                className={`${classes.amount} ${classes.totalDownloadColor}`}
+              >
+                {totalSummary?.total_downloads}
+              </span>
+              <span className={classes.title}>Total Download</span>
+            </div>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default TotalCountHistory;
