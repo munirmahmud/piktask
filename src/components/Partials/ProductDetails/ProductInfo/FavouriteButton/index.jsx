95d2faa5f2c7ab800a3507e67a07688b6602908f
prantoshikder <prantoshikder24@gmail.com>
2021-11-15 15:39:31 +0600

    code updated

---
+++ src/components/Partials/ProductDetails/ProductInfo/FavouriteButton/index.jsx
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({}));
+
+const FavouriteButton = () => {
+  const classes = useStyles();
+  return <div></div>;
+};
+
+export default FavouriteButton;
